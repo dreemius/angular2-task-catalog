27330815d08b8bd627dbdf0010d339314864b566
Eugene <dreemius@gmail.com>
2017-04-23 13:16:17 +0300

    Remove maps and compiled js files

--- client/src/app/models/task.interface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UrgencyMap = [{ code: 'warn', name: "Срочно" }, { code: 'accent', name: "Нормально" }, { code: 'primary', name: "Не срочно" }];
-//# sourceMappingURL=task.interface.js.map

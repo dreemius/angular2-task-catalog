7bc490562a3fee10b0b4ec3f18fe1c3da91e1fa3
Eugene <dreemius@gmail.com>
2017-05-04 17:48:26 +0300

    Add comment to router. Version2

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { StatisticsComponent } from "./statistics/statistics.component";
 import { TaskComponent } from "./task/task.component";
 import { TaskEditComponent } from "./task/edit/task-edit.component";
 
-
+/** another best comment */
 export const routes: Routes = [
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
     { path: "dashboard",    component: DashboardComponent },
